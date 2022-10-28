936b52ca79283efa5d207301fdbbf11d839bfa85
franciscalaureano <76536458+franciscalaureano@users.noreply.github.com>
2022-10-28 14:49:19 +0100

    color blindness + teste audio a funcionar

---
+++ js/audio.js
@@ -0,0 +1,19 @@
+let img = document.getElementById("img");
+var x = document.getElementById("myAudio");
+
+function playAudio() {
+  x.play();
+}
+
+function pauseAudio() {
+  x.pause();
+}
+
+/*var audio = document.getElementById("myAudio");
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}*/

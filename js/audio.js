6b87fb6cd00a9a89172f2224bf224fb2c7616d8a
franciscalaureano <76536458+franciscalaureano@users.noreply.github.com>
2022-10-31 11:26:31 +0000

    alterações do audio e do código no html e javascript

--- js/audio.js
+++ js/audio.js
@@ -3,16 +3,34 @@ var audio2 = document.getElementById("audio2");
 var audio3 = document.getElementById("audio3");
 var audio4 = document.getElementById("audio4");
 
-function playAudio() {
+function playAudio1() {
   audio1.play();
+}
+
+function pauseAudio1() {
+  audio1.pause();
+}
+
+function playAudio2() {
   audio2.play();
+}
+
+function pauseAudio2() {
+  audio2.pause();
+}
+
+function playAudio3() {
   audio3.play();
+}
+
+function pauseAudio3() {
+  audio3.pause();
+}
+
+function playAudio4() {
   audio4.play();
 }
 
-function pauseAudio() {
-  audio1.pause();
-  audio2.pause();
-  audio3.pause();
+function pauseAudio4() {
   audio4.pause();
 }
